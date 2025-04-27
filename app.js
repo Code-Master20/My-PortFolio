a1f3176a50c40d92827fada91b8d7a8b37b0a336
Code-Master20 <sahidurmiah201920@gmail.com>
2025-04-27 22:28:07 +0530

    First Routing Done (clientRouter.js)

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+const { clientRouter } = require("./routes/clientRouter"); // Importing the client router
+const path = require("path");
+const rootDir = require("./utils/pathUtils");
 const express = require("express");
 
 const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.urlencoded({ extended: false })); // Middleware to parse URL-encoded data
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use(clientRouter); // Use the clientRouter for handling requests
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
